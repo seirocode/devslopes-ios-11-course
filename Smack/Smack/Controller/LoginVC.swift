f2bb064fae83314e68a013c402202679b56b1b49
Nadia <nadia@Nadias-iMac.attlocal.net>
2017-10-03 03:05:32 -0700

    lesson 4

---
+++ Smack/Smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by Nadia on 10/3/17.
+//  Copyright © 2017 Nadia. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  smack
+//
+//  Created by Nadia on 10/3/17.
+//  Copyright © 2017 Nadia. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
